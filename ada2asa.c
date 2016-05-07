fec1ca229906e2efd0d335afc62fd1af9aa00f67
Gustavo Fragoso <gbfragoso94@gmail.com>
2016-05-07 15:03:24 -0300

    Update ada2asa.c
    Updating the function ada_to_asa cause in some situations the previous approach do infinite loop. This version do not cause any trouble

--- ada2asa.c
+++ ada2asa.c
@@ -32,33 +32,31 @@ void applyProduction(char *array, int production, int node){
     }
 }
 
-int getLastNode(int visitedNodes[]){
-    int i, max = 0;
-    for(i=0;i<50;i++){
-        if(visitedNodes[i] > max){
-            max = visitedNodes[i];
-        }
+int getFather(int node){
+    int father = 0;
+    if ((node-1)%2 == 0){
+        father = (node-1)/2;
+    }else{
+        father = (node-2)/2;
     }
-    return max+1;
+    return father;
 }
 
 void ada_to_asa(char *array, int productions[]){
     int next = 0, i, j = 0;
-    int visitedNodes[50] = { -1 };
     
     for(i=0; productions[j] != -1 ; i++){
         switch(array[next]){
             case 'E':
                 // The leftmost node will receive production
                 applyProduction(array,productions[j++],next);
-                visitedNodes[i] = next;
                 
                 if(array[2*next+1] == 'E'){
                     next = 2*next+1;
                 }else if(array[2*next+2] == 'E'){
                     next = 2*next+2;
                 }else{
-                    next = visitedNodes[i-1];
+                    next = getFather(next);
                 }
             break;
             case 'A':
@@ -68,11 +66,16 @@ void ada_to_asa(char *array, int productions[]){
                 }else if(array[2*next+2] == 'E'){
                     next = 2*next+2;
                 }else{
-                    next = visitedNodes[i-2];
+                    next = getFather(next);
                 }
             break;
+            default:
+                next = getFather(next);
+        }
+        if(next > max){
+            max = next; // Catch last node position in array
         }
     }
 
-    array[getLastNode(visitedNodes)] = '\0';
+    array[max + 1] = '\0';
 }
