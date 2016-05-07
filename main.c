88f48cdc8b00077f99c91200f0899e27e0315f1f
Gustavo Fragoso <gbfragoso94@gmail.com>
2016-05-07 15:26:35 -0300

    Update main.c
    Trying to fix bad github identation

--- main.c
+++ main.c
@@ -15,12 +15,16 @@ typedef char * string;
 
 int main (int argc, char **argv){
 	
-	int stack[100] = {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3};		// Vector with only integers, used as a datastore
-    int productions[50], i;	    							// Result of Pushdown Automata
-    char btree[50] = {""};									// Binary Tree
-    string rpn;               								// Reverse Polish Notation
-    
-    FILE *input;
+	// Vector with only integers, used as a datastore
+	int stack[100] = {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3};
+	// Result of Pushdown Automata
+	int productions[50], i;
+	// Binary Tree
+	char btree[50] = {""};
+	// Reverse Polish Notation
+	string rpn;
+	
+	FILE *input;
     
     for (i = 0;i<50;i++){
         btree[i] = '_';
@@ -34,49 +38,47 @@ int main (int argc, char **argv){
     	// Expr
     	string expr = argv[1];
     	int size = strlen(expr);
-
-		if(strlen(expr)<50){
-			FILE *output = fopen("output.txt", "w");
-		    if(output!=NULL){
-		    	fprintf(output,"Run: %s %s \n", argv[0], argv[1]);
-
-				printf("Step 1. Catch regular expression ... done!\n");
-				
-				// Execution of Pushdown Automata results in set of productions
-				printf("Step 2. Running pushdown automata ... ");
-				pushDownAutomata(expr,productions,output);
-				printf("done!\n");
-				
-				// Apply productions to build ASA
-				printf("Step 3. Apply productions to build an btree ... ");
-				ada_to_asa(btree,productions);
-				fprintf(output,"\nBtree: %s", btree);
-				printf("done!\n");
-                
-			    // B-tree -> Reverse Polish Notation
-                printf("Step 4. Running postorder trasversal on btree ... ");
-                rpn = (string) malloc(50*sizeof(string));
-			    postOrder(btree,size, 0, rpn);
-			    fprintf(output,"\nReverse polish notation: %s\n", rpn);
-			    printf("done!\n");	
-				
-			    // RPN -> PCI
-			    printf("Step 5. Convert RPN into pcode instructions ... ");
-				Tinstruction * instructions = toInstruction(rpn, size);
-				printf("done!\n");
-				
-				// Array of instructions
-				printf("Step 6. Running pcode machine ... ");
-				fprintf(output,"\nOutput:\n");
-				fprintf(output,"%-10s %-7s %-7s %-7s %-7s %-15s %s\n","Inst","Level","Arg","Top","Counter","Base","Stack");
-				pcode(instructions, stack, output);
-				printf("done!\n");
-				
-				free(rpn);
+    	
+    	if(strlen(expr)<50){
+    		FILE *output = fopen("output.txt", "w");
+    		if(output!=NULL){
+    			fprintf(output,"Run: %s %s \n", argv[0], argv[1]);
+    			printf("Step 1. Catch regular expression ... done!\n");
+    			// Execution of Pushdown Automata results in set of productions
+    			printf("Step 2. Running pushdown automata ... ");
+    			pushDownAutomata(expr,productions,output);
+    			printf("done!\n");
+    			
+    			// Apply productions to build ASA
+    			printf("Step 3. Apply productions to build an btree ... ");
+    			ada_to_asa(btree,productions);
+    			fprintf(output,"\nBtree: %s", btree);
+    			printf("done!\n");
+    			
+    			// B-tree -> Reverse Polish Notation
+    			printf("Step 4. Running postorder trasversal on btree ... ");
+    			rpn = (string) malloc(50*sizeof(string));
+    			postOrder(btree,size, 0, rpn);
+    			fprintf(output,"\nReverse polish notation: %s\n", rpn);
+    			printf("done!\n");
+    			
+    			// RPN -> PCI
+    			printf("Step 5. Convert RPN into pcode instructions ... ");
+    			Tinstruction * instructions = toInstruction(rpn, size);
+    			printf("done!\n");
+    			
+    			// Array of instructions
+			    printf("Step 6. Running pcode machine ... ");
+			    fprintf(output,"\nOutput:\n");
+			    fprintf(output,"%-10s %-7s %-7s %-7s %-7s %-15s %s\n","Inst","Level","Arg","Top","Counter","Base","Stack");
+			    pcode(instructions, stack, output);
+			    printf("done!\n");
+			    
+			    free(rpn);
 			    // Closing File
-		    	fclose(output);
-		    	
-		    	printf("Output file generated.\n");	
+			    fclose(output);
+			    
+			    printf("Output file generated.\n");	
 		    }else{
 		    	printf("\nError when opening the output file.");
 		    }
